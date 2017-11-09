b7af543968e02828a0a3b8321ab1767f1b5a624a
wenshantang <wenshan.tang@sao.so>
2017-11-09 21:19:26 +0800

    11

--- webapp/portal/js/cn_global_1.js
+++ webapp/portal/js/cn_global_1.js
@@ -31,20 +31,17 @@ menuHighlight.exec = function(){
 };
 
 //----------------------------------------------
-//add by bob 2011-04-07 (start)
 var mouse_events = {};
 
 $(document).ready(function(){    
     if(!iPx()){
         mouse_events.mouse_over();
-        mouse_events.mouse_out();
     }
 });
 
 var timeout;
 mouse_events.mouse_over = function(){
     $("#menu li").mouseover(function(){
-        //onmouseover&#26102;&#38388;
         dateIn = new Date();
         timeIn = dateIn.getTime();
         timeOut = 0;
@@ -55,29 +52,10 @@ mouse_events.mouse_over = function(){
     });
 };
 
-
-mouse_events.mouse_out = function(){
-    $("#menu li").mouseout(function(){                                  
-        //onmouseout&#26102;&#38388;
-        dateOut = new Date();
-        timeOut = dateOut.getTime();
-        timeIn = 0;
-        flag = false;   
-        
-        if (divShow_flag != true) {
-            $("#menu li a").removeClass('hover');
-        }
-        //alert("sdf");
-        clearTimeout(timeout);
-        clearTimeout(time_temp);
-    });
-};
-
 mouse_events.aShow = function(classname){
 
     $('#menu .'+classname+' a').addClass('hover');  
 };
-
 
 var dateOut;
 var timeOut;
@@ -109,27 +87,17 @@ menuPopup.ini = function(){
     
         menuPopup.stop();
         $('#menu li a').removeClass('active');
-        $('#menu li a').removeClass('hover');
         menuPopup.obj.find('.popup').hide();
-        
-        //alter by bob 2011-04-07
-        //var target = this.parentNode.className;
         target = this.parentNode.className;
-        
         var position = $(this).offset();
         var shadowOffset = 2;
         if ($.browser.msie && parseInt($.browser.version) == 6) 
             shadowOffset = 0;
-       
-        //alter by bob 2011-04-07
-        //var css = {
         css = {
             top: position.top + 49,
             left: $('#menu li a').eq(0).offset().left - shadowOffset
         };
 
-        //---------------------------------------------------
-        //flag&#21028;&#26029;&#26159;&#21542;&#40736;&#26631;&#22312;MENU&#19978;&#20572;&#30041; (add by bob 2011-04-07) (start)
         if(iPx()){
         
             var $target = menuPopup.obj.css(css).find('.' + target).show();
@@ -142,16 +110,6 @@ menuPopup.ini = function(){
                 time_temp = setTimeout(showDiv, 500);       
             }
         }
-    
-/*       
-        var $target = menuPopup.obj.css(css).find('.' + target).show();
-         
-       
-        if ($target.length > 0) 
-            $(this).addClass('hover');
-*/   
-        //(end)
-        //---------------------------------------------------
         if ($(this).parent().attr('class') != $('#menu').data("normal")) {
             menuHighlight.exec();
         }
@@ -163,7 +121,6 @@ menuPopup.ini = function(){
 };
 menuPopup.stop = function(){
 
-    //add by bob 2011-04-07
     divShow_flag = true;
 
     if (menuPopup.timer) 
@@ -173,7 +130,6 @@ menuPopup.hide = function(){
     menuPopup.stop();
     menuPopup.timer = setTimeout(function(){
         menuPopup.obj.css('top', '-1000px');
-        $('#menu a').removeClass('hover');
         menuHighlight.exec();
     }, 10);
 }
